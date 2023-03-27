baa7529ec2ca5e36af35c06bca1c9993f15d2a52
Ans de Nijs <ans@chordify.net>
2023-03-27 11:35:53 +0200

    Cleanup old non-working code

--- app/routes/artists/artistSongs/$artistSongs.tsx
+++ app/routes/artists/artistSongs/$artistSongs.tsx
@@ -1,11 +1,8 @@
 import type { LinksFunction, LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
-import type { FormEvent } from "react";
-import React from "react";
+import { Form, useLoaderData } from "@remix-run/react";
 import { getArtistSongs } from "~/models/artists.server";
 import stylesUrl from "~/styles/songs.css";
-import { useLocation, redirect } from "react-router-dom";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: stylesUrl }];
@@ -17,30 +14,22 @@ type LoaderData = {
 
 export const loader: LoaderFunction = async ({params}) => {
   return json<LoaderData>({
-    data: await getArtistSongs(params.artist),
+    data: await getArtistSongs(params.artistSongs),
   });
 };
 
 export default function Artist() {
-  let location = useLocation();
-  console.log({location});
-  const [artistName, setArtistName] = React.useState(location);
   const { data } = useLoaderData() as LoaderData;
-  const onSearch = (e: FormEvent) => {
-    console.log('triggered');
-	  e.preventDefault();
-    redirect(`/${artistName}`);
-}
 
   return (
     <main>
       <h1>
         From which artist do you want songs?
 	  </h1>
-	<form onSubmit={onSearch} className="search-form">
-		{/* <input name="artist-name" value={artistName} onChange={(e) => setArtistName(e.target.value)} /> */}
+	<Form className="search-form">
+		<input name="artistName" type="text" />
 		<button type="submit">Search!</button>
-	</form>
+	</Form>
       <div className="container">
         {data.map((song) => (
           <figure key={song.slug}>
